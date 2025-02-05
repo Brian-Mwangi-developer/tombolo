dfd7d9b96f62164301fbb6babee61784cf417892
Brian-Mwangi-Developer <britabyzk29@gmail.com>
2025-02-05 14:15:08 +0300

    mumo

--- next.config.js
+++ next.config.js
@@ -1,27 +1,11 @@
-import CopyWebpackPlugin from 'copy-webpack-plugin';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config, { isServer }) {
-    config.plugins.push(
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: './node_modules/@deriv/deriv-chart/dist/*.smartcharts.*',
-            to: 'public/dist/[name][ext]',
-            flatten: true,
-          },
-          {
-            from: './node_modules/@deriv/deriv-chart/dist/smartcharts.css',
-            to: 'public/dist/smartcharts.css',
-            flatten: true,
-          },
-        ],
-      })
-    );
+    // No need to use CopyWebpackPlugin since files are already in public/dist
     return config;
   },
 };
 
 export default nextConfig;
+
 
